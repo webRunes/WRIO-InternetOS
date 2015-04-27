dbdea3ca421761a1d1aa398db7e1e35a2454b2f0
madhu-lemosys <madhusudan.lemosys@gmail.com>
2015-04-27 18:24:24 +0530

    wrio.js updated

--- WRIO.js
+++ WRIO.js
@@ -23,34 +23,50 @@ loadScripts([
    "http://wrio.s3-website-us-east-1.amazonaws.com/WRIO-InternetOS/require.js"
 ],function(){
     require.config({
-            waitSeconds : 0,
-            baseUrl: "scripts/",
-          
-            paths: {
-            "jquery": "jquery.min",
-            "bootstrap":"bootstrap.min",
-            "react": "react",  
-            "JSXTransformer": "JSXTransformer",
-            "showdown": "showdown.min",
-            "plus":"http://codingserver.com/react/Plus-WRIO-App-master/plus",
-            "client":"client",
-            "promise":"promise-1.0.0.min",
-            "wrio": "WRIODEMO"
-            },   
-            jsx: {
-            fileExtension: '.jsx',
-            harmony: true,   
-            stripTypes: true
-            }, shim : {
-              "bootstrap" : { "deps" :['jquery'] }
-            },
-          });
-        require(['react', 'jsx!wrio'], function(React, CreateDom) { 
-        // Mount the JSX component in the app container 
-        React.render(
-            CreateDom(),            
-            document.body);
-        });
+      config: {
+        text: {
+            useXhr: function (url, protocol, hostname, port) {
+                //Override function for determining if XHR should be used.
+                //url: the URL being requested
+                //protocol: protocol of page text.js is running on
+                //hostname: hostname of page text.js is running on
+                //port: port of page text.js is running on
+                //Use protocol, hostname, and port to compare against the url
+                //being requested.
+                //Return true or false. true means "use xhr", false means
+                //"fetch the .js version of this resource".
+        return true;
+            }
+        }
+    },
+waitSeconds : 0,
+baseUrl: "http://wrio.s3-website-us-east-1.amazonaws.com/WRIO-InternetOS/",
+
+  paths: {
+  "jquery": "jquery.min",
+  "bootstrap":"bootstrap.min",
+    "react": "react",   
+    "JSXTransformer": "JSXTransformer",
+    "showdown": "showdown.min",
+    "wrio": "WRIO",
+  "client":"http://wrio.s3-website-us-east-1.amazonaws.com/Plus-WRIO-App/js/client",
+  "promise":"http://wrio.s3-website-us-east-1.amazonaws.com/Plus-WRIO-App/js/promise-1.0.0.min",
+    "plus":"http://wrio.s3-website-us-east-1.amazonaws.com/Plus-WRIO-App/js/plus",
+  },   
+  jsx: {
+    fileExtension: '.jsx',
+    harmony: true,  
+    stripTypes: true
+  }
+});
+
+require(['react', 'jsx!wrio'], function(React, CreateDom) { 
+// Mount the JSX component in the app container 
+React.render(
+    CreateDom(),            
+    document.body);
+});
+
 });
 
 
