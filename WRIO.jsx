8923c235c392f10650f0d2d544918312ec18e071
spirinvladimir <spirin.vladimir@gmail.com>
2015-05-22 17:07:27 +0300

    resolved merge conflict

--- WRIO.jsx
+++ WRIO.jsx
@@ -1,4 +1,4 @@
-define(['react','plus','jsx!titter','moment','promise','client','jquery','bootstrap','showdown'], function(React,plus,CreateTitter,moment) {
+define(['react','plus','jsx!titter', 'login','promise','client','jquery','bootstrap','showdown'], function(React,plus,CreateTitter,Login) {
 /**  
  * This file provided by Facebook is for non-commercial testing and evaluation purposes only.  
  * Facebook reserves all rights not expressly granted.  
@@ -425,102 +425,6 @@ var CreateArticleList = React.createClass({
   render: function() {
     return (
     	<CreateArticle data={this.state.data} />
-    );
-  }
-});
-
-
-var Details = React.createClass({
-  getInitialState: function() {
-    return {
-      img: importUrl + theme + '/img/no-photo-200x200.png',
-      registered: moment(Date.now()).format('DD MMM YYYY')
-    };
-  },
-  render: function() {
-    return (
-      <div className="col-xs-12 col-md-6 pull-right">
-          <span itemscope itemtype="http://schema.org/ImageObject">
-              <img itemprop="thumbnail" src={this.state.img} className="pull-left" />
-          </span>
-          <ul className="details">
-              <li>Registered: {this.state.registered}</li>
-              <li>Rating: {this.state.rating}</li>
-              <li>Followers: {this.state.followers}</li>
-              <li>Posts: {this.state.posts}</li>
-          </ul>
-      </div>
-    );
-  }
-});
-
-var Login = React.createClass({
-  getInitialState: function() {
-    return {
-      title: {
-        text: "Logged as I'm Anonymous ",
-        label: 'WRIO'
-      },
-      upgrade: {
-        text: 'Upgrade guest account for free',
-        label: '30 days left'
-      },
-      have: {
-        text: 'Already have an account?'
-      },
-      twitter: {
-        url: 'http://login.twitter.com/auth/twitter',
-        img: 'http://www.foodini.co/assets/sign-in-with-twitter-icon-4ab300ee57991db4bd4b4517c5b8e9ed.jpg'
-      }
-    };
-  },
-    componentDidMount: function () {
-    var that = this;
-    window.addEventListener('message', function (e) {
-        var message = e.data;
-        console.log("Got message from iframe", message);
-        var jsmsg = JSON.parse(message);
-        that.setState({
-            upgrade: {
-                text: "Upgrade guest account for free",
-                label: days + 'days left',
-                url: jsmsg.url
-            }
-        });
-
-    });
-    },
-  render: function() {
-    return (
-      <ul className="info nav nav-pills nav-stacked" id="profile-accordion">
-          <li className="panel">
-              <a href="#profile-element" data-parent="#profile-accordion" data-toggle="collapse">
-                <span className="glyphicon glyphicon-chevron-down pull-right"></span>{this.state.title.text}<sup>{this.state.title.label}</sup>
-              </a>
-              <div className="in" id="profile-element">
-                  <div className="media thumbnail">
-                      <Details />
-                      <div className="col-xs-12 col-md-6">
-                          <p>{this.state.description}</p>
-                          <ul className="actions">
-                              <li>
-                                <iframe src="http://storage.webrunes.com" style="display:none"></iframe>
-                                <a href="wrio-account-edit.htm"><span className="glyphicon glyphicon-arrow-up"></span>{this.state.upgrade.text}</a> <span className="label label-warning">{this.state.upgrade.label}</span>
-                              </li>
-                          </ul>
-                          <ul className="actions">
-                              <li>
-                                <a href="#"><span className="glyphicon glyphicon-user"></span>{this.state.have.text}</a>
-                              </li>
-                          </ul>
-                          <a href={this.state.twitter.url}>
-                            <img src={this.state.twitter.img} width="150" height="30" className="twitter" />
-                          </a>
-                      </div>
-                  </div>
-              </div>
-          </li>
-      </ul>
     );
   }
 });
