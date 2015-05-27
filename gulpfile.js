f4762c6ec05b4d9d8e7a2f8f822d91e586a4905c
vladimir.s <vladimir.s@ezscratch.com>
2015-05-27 22:40:11 +0300

    Uninstall modules after build bundle

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,30 @@
 var gulp = require('gulp'),
 	download = require("gulp-download"),
+	npm = require("npm"),
+	package = require('./package.json'),
 	server = 'http://wrio.s3-website-us-east-1.amazonaws.com/';
 
 gulp.task('default', ['download']);
+
+gulp.task('clear', function () {
+	var m = [
+		"react-tools",
+		"reactify",
+		"watchify",
+		"uglifyify",
+		"browserify",
+		"browserify-notify",
+		"browserify-shim",
+		"envify",
+		"gulp-download",
+		"gulp"
+	];
+	npm.load({}, function () {
+		npm.commands.uninstall(
+			Object.keys(package.dependencies).concat(m)
+		);
+	});
+});
 
 gulp.task('debug', ['debugServer', 'download']);
 
