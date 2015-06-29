4764096dc03080fbb4a23312e0b846b5cf113ed3
Umair A. Shahid <umair@inbox.com>
2015-06-29 16:30:54 +0500

    Configuring MongoDB for OpsWorks

---
+++ mongodb-opsworks/metadata.rb
@@ -0,0 +1,7 @@
+name        "mongodb-opsworks"
+description 'Example cookbook for MongoDB Berkshelf based install'
+maintainer  "WebRunes"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
