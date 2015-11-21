a67925a64ab1c3c4920d8bde2df856773241ed3e
Alexey Anshakov <AlexeyAnshakov@users.noreply.github.com>
2015-11-21 21:32:07 +0100

    Revert "Website #192 Hide comments for tweet send page"

--- WRIO-InternetOS/js/components/CreateDomCenter.js
+++ WRIO-InternetOS/js/components/CreateDomCenter.js
@@ -107,13 +107,11 @@ class CreateDomCenter extends React.Component{
 
         var displayCore = '';
         var displayWebgold = '';
-        var nocomments = false;
 
         var notDisplayCenter = false;
         switch (window.location.search) {
             case '?create':
                 condition = false;
-                nocomments = true;
                 notDisplayCenter=true;
                 break;
             case '?add_funds':
@@ -145,7 +143,7 @@ class CreateDomCenter extends React.Component{
                     { displayCore }
                     { displayWebgold }
                     <div style={{display: condition ? 'none' : 'block'}}>
-                        <CreateTitter scripts={this.props.data} nocomments={ nocomments }/>
+                        <CreateTitter scripts={this.props.data} />
                     </div>
                 </div>
             </div>
