9c76c90893df58b57e430027c23e66a3aaf09fa8
Michael Bilenko <denso.ffff@gmail.com>
2015-11-23 19:05:23 +0200

    back gzip

--- compress.sh
+++ compress.sh
@@ -6,14 +6,14 @@ rm -fr node_modules
 
 echo "Gzipping scripts"
 
-#gzip -9 start.js
-#mv start.js.gz start.js
+gzip -9 start.js
+mv start.js.gz start.js
 
-#gzip -9 start.js.map
-#mv start.js.map.gz start.js.map
+gzip -9 start.js.map
+mv start.js.map.gz start.js.map
 
-#gzip -9 main.js
-#mv main.js.gz main.js
+gzip -9 main.js
+mv main.js.gz main.js
 
-#gzip -9 main.js.map
-#mv main.js.map.gz main.js.map
+gzip -9 main.js.map
+mv main.js.map.gz main.js.map
