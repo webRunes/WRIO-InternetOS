bce805718e0d7b6cb19ef7f4106d7aeba711c78b
Andrew <koreshkov.andrey@gmail.com>
2016-01-05 17:07:58 +0300

    ImageObject in article #111
    https://github.com/webRunes/Default-WRIO-Theme/issues/111

--- WRIO-InternetOS/js/jsonld/image.js
+++ WRIO-InternetOS/js/jsonld/image.js
@@ -1,30 +1,32 @@
-var Image = function(opts) {
-	this.name = opts.name;
-	this.description = opts.description;
-	this.url = opts.contentUrl;
-	var cutUrl = this.url.split('?'),
-		positions = cutUrl[1].split(',');
-	this.newUrl = cutUrl[0];
-	this.order = Number(positions[0]);
-	this.start = Number(positions[1]);
-};
+class Image {
+	constructor(opts) {
+		this.name = opts.name;
+		this.description = opts.description;
+		this.url = opts.contentUrl;
+		var cutUrl = this.url.split('?'),
+			positions = cutUrl[1].split(',');
+		this.newUrl = cutUrl[0];
+		this.order = Number(positions[0]);
+		this.start = Number(positions[1]);
+	}
 
-Image.prototype.warn = function(text) {
-	text = text || 'Wrong image: ' + this.url;
-};
+	warn(text) {
+		text = text || 'Wrong image: ' + this.url;
+	}
 
-Image.prototype.attach = function(s) {
-	var before = s.substr(0, this.start),
-		after = s.substring(this.start, s.length);
-	return {
-		before: before,
-		image: {
-			name: this.name,
-			description: this.description,
-			src: this.newUrl
-		},
-		after: after
-	};
-};
+	attach(s) {
+		var before = s.substr(0, this.start),
+			after = s.substring(this.start, s.length);
+		return {
+			before: before,
+			image: {
+				name: this.name,
+				description: this.description,
+				src: this.newUrl
+			},
+			after: after
+		};
+	}
+}
 
 module.exports = Image;
