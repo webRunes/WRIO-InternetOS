7d94a23d36ad545c53a75a5bc0599e211e560334
Michael Bilenko <denso.ffff@gmail.com>
2016-03-22 12:53:38 +0200

    Fix urls

--- WRIO-InternetOS/js/components/Lockup.js
+++ WRIO-InternetOS/js/components/Lockup.js
@@ -4,12 +4,12 @@ import Login from '../../../widgets/Login.jsx';
 
 
 var imgStyle = {
-    background: "url('//webrunes.github.io/Default-WRIO-Theme/img/animated.jpg') center center",
+    background: "url('//default.wrioos.com/img/animated.jpg') center center",
     'backgroundSize': 'cover'
 };
 
 var backStyle = {
-    background: "url('//webrunes.github.io/Default-WRIO-Theme/img/welcome_back-cover.jpg') center center",
+    background: "url('//default.wrioos.com/img/welcome_back-cover.jpg') center center",
     'backgroundSize': 'cover'
 };
 
