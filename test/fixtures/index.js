85bb395c4cc86b415b254916a33f60212762661e
Michael Bilenko <denso.ffff@gmail.com>
2016-09-21 23:07:01 +0300

    missing test file

---
+++ test/fixtures/index.js
@@ -0,0 +1,144 @@
+﻿const fixture = [
+
+    {
+        "@context": "https://schema.org",
+        "@type": "ItemList",
+        "name": "webRunes cover list",
+        "itemListElement": [
+            {
+                "@type": "ItemList",
+                "name": "Cover",
+                "description": "webRunes cover",
+                "image": "https://default.wrioos.com/img/no-photo-200x200.png",
+                "url": "https://webrunes.com/cover/?cover"
+            },
+            {
+                "@type": "ItemList",
+                "name": "Offers",
+                "description": "webRunes offers list",
+                "image": "https://default.wrioos.com/img/no-photo-200x200.png",
+                "url": "https://webrunes.com/offer/?cover"
+            }
+        ]
+    },
+
+            {
+                "@context": "https://schema.org",
+                "@type": "Article",
+                "inLanguage": "En",
+                "keywords": "webRunes",
+                "name": "webRunes",
+                "about": "The webRunes team has a 10+ year experience in such spheres like technical and web design, programming and data security, payment systems and some other related spheres. We have started working on the project in September 2010. Our goal is to develop a unique platform that will serve as a basis for different projects that help people in various areas of life from the childhood to the old age: education, healthcare, sport, law and etc. We are against growing trends for Internet censorship and information access restriction, so we use only uncopyrighted data.",
+                "articleBody": [
+                "webRunes is the first contractor for Alternative project. And we are the first ad hoc team of freelancers specializing in various IT fields and other areas of expertise that are developing WRIO Internet OS platform. WRIO Internet OS is a gate to semantic, decentralized and secure Internet. The platform is required for the technical implementation of Alternative — a set of projects aimed at the development of a free self-managed society, an alternative approach to the interaction of people with minimal or even without participation of the government. The technical implementation is presented in the form of interconnected services that help people throughout their lives in different area: education, health, communication, finance and others.",
+                "First thoughts about the project emerged in late May 2010, and already on September 20 the first developer started its work. By that time, the initial service concepts were developed, which later became applications of WRIO Internet OS.",
+                "Using only own founder’s funds, for over four years , we have worked our way up from a bare idea to the first working version, developing several prototypes and making a number of pivots, to eventually find a solid approach to the technical implementation of WRIO OS and answer to the question of Alternative: «what projects should be implemented and, more importantly, how, in order to make this world a better place, or at least inspire others to do try». The idea is not original, but some extra effort on this way is never superfluous. Moreover, it has been decided, what projects are needed to promote of WRIO Internet OS.",
+                "For the future we have more global plans related to the system management by a core based on AI algorithms — IA (Individual Assistant, reverse of AI — artificial intelligence), which will become each user's personal guide to the world of the Internet and will add a variety of extra functions, in particular on the selection of articles, books, music, video and other content. Today, the first version of IA is just a set of scenarios with a given reaction, but in the future, the system will be much more complex and advanced, as you can read in IA hub."
+                ],
+                "hasPart": [
+            {
+                "@type": "Article",
+                "name": "Mission",
+                "articleBody": [
+                "Lifelong learning is our credo, our mission is to enlighten people.",
+                "In a single sentence our technical mission can be described as the development of technologies aimed at the rise of a free and decentralized Internet based on cryptography and blockchain that will be beyond the control of national governments and corporations.",
+                "We develop more than isolated projects with a set of functions. They are conceptually interrelated, which significantly expands the scope of their applicability and enables the synergy – when the aggregated effect substantially surpasses the sum of individual components. The functional is developed in a way to render assistance literally to every user in everyday life. The services will be useful both for private and corporate segments. We started from offering the possibility of creating a private hub and publishing articles from grateful readers with a donate-functionality. As we go forward we’ll extend the functionality to offer content distribution services: audio, video, articles, books, training courses, soft, games, mobile applications and etc. But our primary technological goal is to develop an intelligent system that not only will select interesting materials for users and relevant readers for authors, but also will provide a unique personal secretary service that will act as an assistant and counselor in various everyday personal and professional matters. The system is required to achieve our next longshot goal: to assist users in lifelong learning. In its own turn this is just another step to help users to obtain new knowledge and extend the horizons of their outlook ensuring access to reliable information. That goal is reflected in webRunes logo. We believe that raising the level of education will improve living standards of people and the society in general.",
+                "Free Internet and education are necessary milestones for the implementation of our main conceptual project Alternative."
+                ]
+            },
+            {
+                "@type": "Article",
+                "name": "What we do",
+                "articleBody": [
+                "Coming soon."
+                ]
+            },
+            {
+                "@type": "Article",
+                "name": "Team",
+                "about": "webRunes team.",
+                "url": "https://webrunes.com/team/"
+            },
+            {
+                "@type": "Article",
+                "name": "Join us",
+                "articleBody": [
+                "Coming soon. But please do not hesitate to contact us info@webrunes.com"
+                ]
+            }
+                ],
+                "mentions": [
+            {
+                "@type": "Article",
+                "name": "Alternative contractor",
+                "about": "Description.",
+                "url": "#?'contractor':3,22"
+            },
+            {
+                "@type": "Article",
+                "name": "Alternative",
+                "about": "Alternative.",
+                "url": "https://alternative.webrunes.org/?'Alternative':3,37"
+            },
+            {
+                "@type": "Article",
+                "name": "Ad hoc teams",
+                "about": "Ad hoc is a Latin phrase meaning “for this”. In English, it generally signifies a solution designed for a specific problem or task, non-generalizable, and not intended to be able to be adapted to other purposes.",
+                "url": "#?'ad hoc team':3,79"
+            },
+            {
+                "@type": "Article",
+                "name": "Information technology",
+                "about": "Is the application of computers to store, retrieve, transmit and manipulate data, often in the context of a business or other enterprise. IT is considered a subset of information and communications technology (ICT).",
+                "url": "https://en.wikipedia.org/wiki/Information_technology?'IT':3,130,external"
+            },
+            {
+                "@type": "Article",
+                "name": "WRIO Internet OS",
+                "about": "WRIO Internet OS: web 3.0.",
+                "url": "https://wrioos.com/?'WRIO Internet OS':3,189"
+            },
+            {
+                "@type": "Article",
+                "name": "WRIO OS application list",
+                "about": "WRIO OS application list.",
+                "url": "https://wrioos.com/applications/?'applications':4,203"
+            },
+            {
+                "@type": "Article",
+                "name": "Official IA hub",
+                "about": "Official IA hub.",
+                "url": "https://ia.wrioos.com/?'IA hub':6,547"
+            },
+            {
+                "@type": "Article",
+                "name": "Synergy",
+                "about": "Synergy is the creation of a whole that is greater than the simple sum of its parts. The term synergy comes from the Attic Greek word συνεργία synergia from synergos, συνεργός, meaning «working together».",
+                "url": "https://en.wikipedia.org/wiki/Synergy?'synergy':10,177,external"
+            },
+            {
+                "@type": "Article",
+                "name": "Create a new hub",
+                "about": "Create a new hub.",
+                "url": "https://wrioos.com/create?'creating a private hub':9,177"
+            },
+            {
+                "@type": "Article",
+                "name": "Donate-functionality",
+                "about": "Donate-functionality.",
+                "url": "https://webgold.wrioos.com/tutorials/donate?'a donate-functionality':9,559"
+            },
+            {
+                "@type": "Article",
+                "name": "IA — Individual Assistant",
+                "about": "IA — Individual Assistant.",
+                "url": "https://ia.wrioos.com?'personal secretary':9,956"
+            }
+                ],
+                "author": "",
+                "editor": "",
+                "comment": "746095521694089216"
+
+    }
+];
+export default fixture;
