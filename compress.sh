5dd17307b3cd3d3ef13335bbcf0dd5ccb329ba9d
Michael Bilenko <denso.ffff@gmail.com>
2017-05-29 09:19:39 +0300

    fix compressing disable lint temporarily

--- compress.sh
+++ compress.sh
@@ -6,14 +6,5 @@ cd build
 
 echo "Gzipping scripts"
 
-gzip -9 start.js
-mv start.js.gz start.js
-
-gzip -9 start.js.map
-mv start.js.map.gz start.js.map
-
-gzip -9 main.js
-mv main.js.gz main.js
-
-gzip -9 main.js.map
-mv main.js.map.gz main.js.map
+gzip -9 *.js *.map
+for i in *.gz ; do mv "$i" "${i/.gz/}" ; done
