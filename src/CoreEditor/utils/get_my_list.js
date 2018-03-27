74c7f3f972edf5eaef1bc7e58e2944d68a16bc39
Spirin Vladimir <spirin.vladimir@gmail.com>
2018-03-27 11:05:17 -0400

    Posts list #465
    In case fail respond request few times

--- src/CoreEditor/utils/get_my_list.js
+++ src/CoreEditor/utils/get_my_list.js
@@ -1,11 +1,16 @@
 const
   request = require('superagent'),
-  html2list = require('./html2list');
+  html2list = require('./html2list'),
+  intentCount = 3,
+  getMyList = (wrioID, times, cb) =>
+    request('GET', 'https://s3.amazonaws.com/wr.io/' + wrioID + '/list.html')
+      .end((err, data) =>
+        err
+          ? times
+            ? setInterval(() => getMyList(wrioID, times - 1, cb), 100)
+            : cb(err)
+          : cb(null, html2list(data.text))
+      );
 
 module.exports = (wrioID, cb) =>
-  request('GET', 'https://s3.amazonaws.com/wr.io/' + wrioID + '/list.html')
-    .end((err, data) =>
-      err
-        ? cb(err)
-        : cb(null, html2list(data.text))
-    )
+  getMyList(wrioID, intentCount, cb);
