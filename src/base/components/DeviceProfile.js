57de0e5be2a44b82f066b6c9d5e7c4010688099f
Alexey Anshakov <info@webrunes.com>
2019-07-06 09:22:27 +0200

    markup for device profile tab has been justified

--- src/base/components/DeviceProfile.js
+++ src/base/components/DeviceProfile.js
@@ -11,25 +11,21 @@ class DeviveProfileTab extends React.Component {
         let productData = this.props.sensorProductData;
         let providerLink = feedData.provider != undefined ? feedData.provider: undefined;
         return (
-           productData !=undefined ? <div className="container">
+           productData !=undefined ? <div>
             {
              providerLink !=undefined ?<ProviderLink providerLink={providerLink}/>:null
             }
-              <div className="paragraph">
-                <div className="col-xs-12">
-                  <div>Product ID: {productData ? productData.productID: ''}</div>
-                  <div>Name: {productData? productData.name: ''}</div>
-                  <div>Description: {productData? productData.description: ''}</div>
-                  <div>Brand: {productData? productData.brand: ''}</div>
-                  <div>Manufacturer: {productData? productData.manufacturer: ''}</div>
-                  <div>Production Date: {productData? productData.productionDate: ''}</div>
-                  <div>Purchase Date: {productData? productData.purchaseDate: ''}</div>
-                  <div>Release Date: {productData? productData.releaseDate: ''}</div>
-                  <div>Height: {productData? productData.height: ''}</div>
-                  <div>Weight: {productData? productData.weight: ''}</div>
-                  <div>Width: {productData? productData.width: ''}</div>
-                </div>
-              </div>
+              <div>Product ID: {productData ? productData.productID: ''}</div>
+              <div>Name: {productData? productData.name: ''}</div>
+              <div>Description: {productData? productData.description: ''}</div>
+              <div>Brand: {productData? productData.brand: ''}</div>
+              <div>Manufacturer: {productData? productData.manufacturer: ''}</div>
+              <div>Production Date: {productData? productData.productionDate: ''}</div>
+              <div>Purchase Date: {productData? productData.purchaseDate: ''}</div>
+              <div>Release Date: {productData? productData.releaseDate: ''}</div>
+              <div>Height: {productData? productData.height: ''}</div>
+              <div>Weight: {productData? productData.weight: ''}</div>
+              <div>width: {productData? productData.width: ''}</div>
             </div>: null
         );
     }
