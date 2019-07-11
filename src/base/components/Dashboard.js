37cf342c8f200db6aaa7a98871f3e6d3cf180b87
WRIOdev <info@wr.io>
2019-07-11 07:23:31 +0500

    modal added on Add new Device button

--- src/base/components/Dashboard.js
+++ src/base/components/Dashboard.js
@@ -1,20 +1,49 @@
 import React from 'react';
 import { connect } from "react-redux";
+import Modal from 'react-awesome-modal';
 
 export class Dashboard extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      visible : false
   }
+  }
+
+  openModal() {
+    this.setState({
+        visible : true
+    });
+}
+
+closeModal() {
+    this.setState({
+        visible : false
+    });
+}
 
   render() {
     let sensorData = this.props.sensorData;
     return (<div>
+            <section>
+                <Modal visible={this.state.visible} width="600" height="300" effect="fadeInUp" onClickAway={() => this.closeModal()}>
+                    <div className="dashboard-modal-div">
+                        <h4>The functionality is available to Alpha testers only.</h4>
+                        <p >Press "Connect" on WRIO IoT Gateway to connect a new device, or enter the IPv6 address and AES key manually. For more information check manual inside the box.</p>
+                        <p >IPv6 [2001:0db8:85a3:0000:0000:****:****:****]</p>
+                        <p >AES key [**********************************]</p>
+                        <a className="btn btn-default dashboard-modal-div-a1" href="javascript:void(0);" onClick={() => this.closeModal()}>Cancel</a>
+                        <a className="btn btn-success dashboard-modal-div-a2 ht"><span class="tooltip">this is the tooltip alshdgwh gahfguo 
+wfhg fghwoug wugw hgrwuog hwaur guoarwhg rwu</span>Submit</a>
+                    </div>
+                </Modal>
+                </section>
         <div className="callout warning col-xs-12">
           <h5>Dasboard is under development and the functionality is limited. Premium features are available to alpha-testers only.</h5>
         </div>
-        <button type="button" className="btn btn-success"><span class="glyphicon glyphicon-plus with_text"></span>Add New Device</button>
-        <table class="table table-striped">
+        <button type="button" className="btn btn-success" onClick={() => this.openModal()}><span className="glyphicon glyphicon-plus with_text"></span>Add New Device</button>
+        <table className="table table-striped">
           <thead>
             <tr>
               <th>ID</th>
@@ -36,7 +65,7 @@ export class Dashboard extends React.Component {
                   return (<tr>
                     <td><div className="dashboard-sensor-id">{sensorProductData.productID}</div></td>
                     <td><a href={sensor.url.slice(0, sensor.url.length-10)}>{sensorProductData.name}</a></td>
-                    <td className="center">{sensorDataFeed[0].item.variableMeasured.value.toLowerCase() == 'enabled' ? <span class="glyphicon glyphicon-ok-sign icon-success"></span> : <span class="glyphicon glyphicon-remove-sign"></span>}</td>
+                    <td className="center">{sensorDataFeed[0].item.variableMeasured.value.toLowerCase() == 'enabled' ? <span className="glyphicon glyphicon-ok-sign icon-success"></span> : <span className="glyphicon glyphicon-remove-sign"></span>}</td>
                     <td>Read</td>
                     <td>{sensorPayload.dateModified}</td>
                     <td>{sensorDataFeed[1].item.variableMeasured.value} &#8451;</td>
