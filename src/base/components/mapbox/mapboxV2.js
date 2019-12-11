3e68589eba4c0cc168a95538900e4a7268b43a86
Alexey Anshakov <a.a@tutanota.com>
2019-12-11 18:15:59 +0200

    Revert " Mapbox resize event reverted and interval time reduced to 4 seconds"

--- src/base/components/mapbox/mapboxV2.js
+++ src/base/components/mapbox/mapboxV2.js
@@ -38,12 +38,7 @@ class MapBox extends React.Component {
   }
     
     this.map.addControl(new mapboxgl.NavigationControl());
-    let that = this;
-      this.map.on('load', function() {
-          setInterval(() => {
-            that.map.resize();
-          },4000)
-      });
+
   }
 
   render(){
