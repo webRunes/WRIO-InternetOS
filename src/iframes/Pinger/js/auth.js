3685c80ce13e2c3dbb17e7c971c947ee671e56d6
vikas mishra <vikasmishra227@gmail.com>
2020-01-17 21:07:30 +0530

    Update auth.js
    Changed as per comment

--- src/iframes/Pinger/js/auth.js
+++ src/iframes/Pinger/js/auth.js
@@ -1,31 +1,32 @@
-// /**
-//  * Created by michbil on 05.03.17.
-//  */
+/**
+ * Created by michbil on 05.03.17.
+ */
+/*
+import { getCookie } from "./utils.js";
+import { getServiceUrl } from "base/servicelocator";
 
-// import { getCookie } from "./utils.js";
-// import { getServiceUrl } from "base/servicelocator";
+export function openAuthPopup() {
+  var loginUrl = getServiceUrl("login");
+  var callbackurl = "//" + window.location.host + "/callback";
+  window.open(
+    loginUrl + "/authapi?callback=" + encodeURIComponent(callbackurl),
+    "Login",
+    "height=500,width=700"
+  );
+}
 
-// export function openAuthPopup() {
-//   var loginUrl = getServiceUrl("login");
-//   var callbackurl = "//" + window.location.host + "/callback";
-//   window.open(
-//     loginUrl + "/authapi?callback=" + encodeURIComponent(callbackurl),
-//     "Login",
-//     "height=500,width=700"
-//   );
-// }
+export function logoff() {
+  $.ajax("/logoff").success(function(res) {
+    location.reload();
+  });
+}
 
-// export function logoff() {
-//   $.ajax("/logoff").success(function(res) {
-//     location.reload();
-//   });
-// }
-
-// export function checkLoggedIn() {
-//   console.log(document.cookie);
-//   if (!getCookie("sid")) {
-//     openAuthPopup();
-//   } else {
-//     console.log("Cookie exists, aborting");
-//   }
-// }
+export function checkLoggedIn() {
+  console.log(document.cookie);
+  if (!getCookie("sid")) {
+    openAuthPopup();
+  } else {
+    console.log("Cookie exists, aborting");
+  }
+}
+*/
