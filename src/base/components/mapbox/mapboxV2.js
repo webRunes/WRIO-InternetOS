bd3aeef28bb3e504c2e3cadffd64cbaebb7ddd73
vikas mishra <vikasmishra227@gmail.com>
2020-02-04 18:44:31 +0530

    Changed dashboard and sensor info for  #1858

--- src/base/components/mapbox/mapboxV2.js
+++ src/base/components/mapbox/mapboxV2.js
@@ -1,64 +1,108 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const Token = "pk.eyJ1Ijoid2VicnVuZXMiLCJhIjoiY2p5NGY5Nm93MTVlNDNpbzg1N3czdmd5cCJ9.CptajnA9N30WwXNK1EygyA";
+const Token =
+  'pk.eyJ1Ijoid2VicnVuZXMiLCJhIjoiY2p5NGY5Nm93MTVlNDNpbzg1N3czdmd5cCJ9.CptajnA9N30WwXNK1EygyA';
 
 class MapBox extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      map: null
-    }
+      map: null,
+    };
     this.map;
   }
 
   componentDidUpdate() {
-    let filteredGeoCoordinates = this.props.geoCoordinates.filter(item => item.feedUrl).map(item => [item.longitude, item.latitude]);
-    let mapBoxGL = window.mapboxgl || undefined;
-    if(mapBoxGL) {
-    mapboxgl.accessToken = Token;
-    this.map = new mapboxgl.Map({
-      container: this.map || 'hidden-map',
-      style: 'mapbox://styles/mapbox/light-v10',
-      center: filteredGeoCoordinates[0] || this.props.geoCoordinates,
-      zoom: 12
-    })
-   
-    if(filteredGeoCoordinates && filteredGeoCoordinates.length > 0) {
-      filteredGeoCoordinates.map(geoCoord => {
-       new mapboxgl.Marker()
-       .setLngLat(geoCoord)
-       .addTo(this.map);     
-      })  
-     } else {
-      new mapboxgl.Marker()
-      .setLngLat(this.props.geoCoordinates)
-      .addTo(this.map);   
-     }
-  }
-    
-    this.map.addControl(new mapboxgl.NavigationControl());
-    let that = this;
-      this.map.on('load', function() {
-          setInterval(() => {
-            that.map.resize();
-          },4000)
+    const filteredGeoCoordinates = this.props.geoCoordinates
+      .filter(item => item.feedUrl)
+      .map(item => [item.longitude, item.latitude]);
+    const mapBoxGL = window.mapboxgl || undefined;
+    if (mapBoxGL) {
+      mapboxgl.accessToken = Token;
+      this.map = new mapboxgl.Map({
+        container: this.map || 'hidden-map',
+        style: 'mapbox://styles/mapbox/light-v10',
+        center: filteredGeoCoordinates[0] || this.props.geoCoordinates,
+        zoom: 12,
       });
+
+      if (filteredGeoCoordinates && filteredGeoCoordinates.length > 0) {
+        filteredGeoCoordinates.map((geoCoord) => {
+          new mapboxgl.Marker().setLngLat(geoCoord).addTo(this.map);
+        });
+      } else {
+        new mapboxgl.Marker().setLngLat(this.props.geoCoordinates).addTo(this.map);
+      }
+    }
+
+    this.map.addControl(new mapboxgl.NavigationControl());
+    const that = this;
+    this.map.on('load', () => {
+      setInterval(() => {
+        that.map.resize();
+      }, 4000);
+    });
   }
 
-  render(){
-    let geoCoordinates = (this.props.geoCoordinates && this.props.geoCoordinates.length > 0) ? this.props.geoCoordinates: undefined;
+  static mapOnClick(cordinates, description,sensorProductName,checkEnable) {// for popup
+    let filteredGeoCoordinates = [cordinates[0].longitude, cordinates[0].latitude];
+    let Description = description;
+    let ProductName = sensorProductName;
+    let el = document.createElement('tr');
+    el.id = 'marker';
+    Description = Description.toString().split(",").join(" <br /> ");
+    let text;
+    if(checkEnable == "enable"){
+      text = '<div style="background:#99ff99; color:white">'+'<p>'+ProductName+'</p>'+"<div style={{whiteSpace: 'pre-line'}}><p>"+Description+'</p></div>'+'<p>'+filteredGeoCoordinates+'</p>+</div>';
+    }else{
+      text = '<div style="background:#8585ad; color:white">'+'<p>'+ProductName+'</p>'+"<div style={{whiteSpace: 'pre-line'}}><p>"+Description+'</p></div>'+'<p>'+filteredGeoCoordinates+'</p>+</div>';
+    }
+    const mapBoxGL = window.mapboxgl || undefined;
+    if (mapBoxGL) {
+      mapboxgl.accessToken = Token;
+      this.map = new mapboxgl.Map({
+        container: 'map' ,
+        style: 'mapbox://styles/mapbox/light-v10',
+        center: filteredGeoCoordinates,
+        zoom: 18,
+      });
+
+      if (filteredGeoCoordinates && filteredGeoCoordinates.length > 0) {
+          new mapboxgl.Marker().setLngLat(filteredGeoCoordinates).addTo(this.map);
+      } else {
+          new mapboxgl.Marker().setLngLat(this.props.geoCoordinates).addTo(this.map);
+      }
+
+      new mapboxgl.Popup({ offset: 25 })
+                  .setLngLat(filteredGeoCoordinates)
+                  .setHTML(text)
+                  .addTo(this.map);
+    } 
+  }
+
+  render() {
+    const geoCoordinates =
+      this.props.geoCoordinates && this.props.geoCoordinates.length > 0
+        ? this.props.geoCoordinates
+        : undefined;
     console.log('geoCoordinates 1 === ', geoCoordinates);
     console.log('window mapbox 1', window.mapboxgl);
-    return ((geoCoordinates && window.mapboxgl != undefined) ?
-      <div className="mapbox-main-div" id='map' ref={(x) => this.map = x}>
-      </div>: <img id="hidden-map" src="https://default.wrioos.com/img/no-photo-200x200.png" width="200" height="200"/>
-    )
+    return geoCoordinates && window.mapboxgl != undefined ? (
+      <div className="mapbox-main-div" id="map" ref={x => (this.map = x)} />
+    ) : (
+      <img
+        id="hidden-map"
+        src="https://default.wrioos.com/img/no-photo-200x200.png"
+        width="200"
+        height="200"
+      />
+    );
   }
-};
+}
 
 const mapStateToPropsMapBox = state => ({
-  geoCoordinates: state.document.geoCoordinates
+  geoCoordinates: state.document.geoCoordinates,
 });
 
 export const MapBoxGl = connect(mapStateToPropsMapBox)(MapBox);
